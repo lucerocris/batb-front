032d7a30a99cb8eafaaa74b23731f4259e06dbec
Cris Lawrence M. Lucero <s21300337@amos.dcism.org>
2025-11-27 13:51:44 +0800

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://batb-api.test',
+        target: 'https://batb-api.dcism.org',
         changeOrigin: true,
         secure: false,
       }
